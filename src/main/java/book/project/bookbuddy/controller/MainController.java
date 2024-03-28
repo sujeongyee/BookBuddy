9dc74ce36d92f7071ad448777b61a050e1272e31
ddudong <sujeongyee0529@gmail.com>
2024-03-28 19:33:02 +0900

    환경세팅, 메인페이지 작업 중

---
+++ src/main/java/book/project/bookbuddy/controller/MainController.java
@@ -0,0 +1,23 @@
+package book.project.bookbuddy.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/book")
+public class MainController {
+
+    @RequestMapping("/main")
+    public ResponseEntity<Map<String,Object>> getMain(@RequestParam("userId") String leaderId){
+        Map<String,Object> map = new HashMap<>();
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/book/project/bookbuddy/BookBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package book.project.bookbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
