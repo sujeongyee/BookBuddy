c0e3f683890541f56e604bdeb550ad5b58d8d0af
ddudong <sujeongyee0529@gmail.com>
2024-04-01 23:37:34 +0900

    로그인 기능 완성

--- src/main/frontreact/src/App.js
+++ src/main/frontreact/src/App.js
@@ -1,14 +1,17 @@
 
 import {Route, Routes} from "react-router-dom";
 import Main from "./main/Main";
+import {useEffect, useState} from "react";
 
 function App() {
+
+
     return (
         <>
             <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/login" element={<Main loginPage />} />
-                <Route path="/regist" element={<Main registPage />} />
+                <Route path="/" element={<Main/>} />
+                <Route path="/login" element={<Main loginPage/>} />
+                <Route path="/regist" element={<Main registPage/>} />
             </Routes>
         </>
 
--- src/main/frontreact/src/main/Sidebar.js
+++ src/main/frontreact/src/main/Sidebar.js
@@ -1,20 +1,34 @@
 import './sidebar.css';
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
 function Sidebar(){
+
     const [showMenuCategory, setShowMenuCategory] = useState(false); // 카테고리 메뉴 전시 여부
     const [showMenuCategory2, setShowMenuCategory2] = useState(false); // 키워드 메뉴 전시 여부
     const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 저장
+    const navigate = useNavigate();
+    const [userId,setUserId] = useState(sessionStorage.getItem("userId"));
 
+    useEffect(() => {
+        setUserId(sessionStorage.getItem("userId"));
+    }, [sessionStorage.getItem("userId")]);
+
+    const logout = () => {
+        sessionStorage.removeItem("userId");
+        setIsLoggedIn(false);
+        setUserId('');
+        navigate('/');
+    }
 
     return(
         <div className="sidebar">
             <Link to="/" className="Logo">
                 <img src={process.env.PUBLIC_URL + '/imgs/logo.png'} alt="Logo"/>
             </Link>
-            {isLoggedIn ? ( // 로그인 상태에 따라
+            {userId ? ( // 로그인 상태에 따라
                 <div className="myInfo">
-                    <p className="hellop"> </p><p className="hellop"> 님 반갑습니다!</p>
+                    <p className="hellop"> {userId} </p><p className="hellop"> 님 반갑습니다!</p>
+                    <button className="logout-btn" onClick={logout}>로그아웃하기</button>
                 </div>
             ):(
                 <div className="myLogin">
@@ -28,28 +42,28 @@ function Sidebar(){
                         onMouseLeave={() => setShowMenuCategory(false)}>
                         <a className="cateoryA">카테고리 별 추천/리뷰</a>
                         {showMenuCategory && (
-                            <div className="menuCategory2">
+                            <ul className="menuCategory2">
                                 <li><a>소설</a><a>시/에세이</a><a>인문</a></li>
                                 <li><a>가정/육아</a><a>요리</a><a>건강</a></li>
                                 <li><a>경제</a><a>자기계발</a><a>정치/사회</a></li>
                                 <li><a>역사/문화</a><a>중/고등참고서</a></li>
                                 <li><a>기술/공학</a><a>외국어</a><a>과학</a></li>
                                 <li><a>취업/수험서</a><a>여행</a><a>컴퓨터/IT</a></li>
-                            </div>
+                            </ul>
                         )}
                     </li>
                     <li className="cateoryLi2" onMouseEnter={() => setShowMenuCategory2(true)}
                         onMouseLeave={() => setShowMenuCategory2(false)}>
                         <a className="cateoryA2">키워드 별 추천/리뷰</a>
                         {showMenuCategory2 && (
-                            <div className="menuCategory2">
+                            <ul className="menuCategory2">
                                 <li><a>#힐링되는</a><a>#흥미진진</a><a>#스릴있는</a></li>
                                 <li><a>#신선한</a><a>#가독성좋은</a><a>#공감되는</a></li>
                                 <li><a>#유익한</a><a>#따뜻한</a><a>#선물하기좋은</a></li>
                                 <li><a>#위로되는</a><a>#감동적인</a><a>#성공</a></li>
                                 <li><a>#생각하게되는</a><a>#교훈있는</a><a>#눈물</a></li>
                                 <li><a>#공포</a><a>#범죄</a><a>#추리</a></li>
-                            </div>
+                            </ul>
                         )}
                     </li>
                     <li><a>이번 주 베스트 추천/리뷰</a></li>
