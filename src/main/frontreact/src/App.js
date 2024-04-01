3ad8a785fbe24e3a66e77a6bbd1fb5cd8371462e
ddudong <sujeongyee0529@gmail.com>
2024-04-01 17:12:14 +0900

    로그인 기능 1차 완성

--- src/main/frontreact/src/App.js
+++ src/main/frontreact/src/App.js
@@ -4,14 +4,13 @@ import Main from "./main/Main";
 
 function App() {
     return (
-      <>
-          <Routes>
-              <Route path="/" element={<Main/>} />
-              <Route path="/login" element={<Main loginPage/>} />
-              <Route path="/regist" element={<Main registPage/>} />
-          </Routes>
-
-      </>
+        <>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/login" element={<Main loginPage />} />
+                <Route path="/regist" element={<Main registPage />} />
+            </Routes>
+        </>
 
     );
 }
