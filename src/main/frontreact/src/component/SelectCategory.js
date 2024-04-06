094be92d4eac0fe7bd3146c77451a3e14b18b88d
ddu <sujeongyee0529@gmail.com>
2024-04-06 16:00:31 +0900

    회원가입 화면 css (2)

---
+++ src/main/frontreact/src/component/SelectCategory.js
@@ -0,0 +1,18 @@
+
+function SelectCategory(){
+  return(
+    <select className="cate-select">
+      <option key='' value=''>소설</option>
+      <option key='' value=''>시/에세이</option>
+      <option key='' value=''>인문</option>
+      <option key='' value=''>가정/육아</option>
+      <option key='' value=''>요리</option>
+      <option key='' value=''>건강</option>
+      <option key='' value=''>종교</option>
+      <option key='' value=''>기술/공학</option>
+      <option key='' value=''>외국어</option>
+      <option key='' value=''>과학</option>
+    </select>                            
+  );
+}
+export default SelectCategory
