ada7d8d1785466673a262cb8d477308360cb2618
ddu <sujeongyee0529@gmail.com>
2024-06-05 02:14:49 +0900

    0605 피드 완성

--- src/main/frontreact/src/postcomponent/PostList.js
+++ src/main/frontreact/src/postcomponent/PostList.js
@@ -4,6 +4,8 @@ import { useUser } from "../context/UserContext";
 import toast, { Toaster } from 'react-hot-toast';
 import "./postgrid.css";
 import { useLoading } from "../context/LoadingContext";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const PostList = ({type}) => {
   const {userData ,setUserData} = useUser();
   const {userId} = userData;
@@ -16,6 +18,7 @@ const PostList = ({type}) => {
         try {
           showLoading();
           const response = await axios.get(`/book/post/getRcmPostMyPage?id=${userId}&type=list`);
+          console.log(response.data);
           setRecommendPosts(response.data);
           hideLoading();
         } catch(error) {
@@ -54,25 +57,30 @@ const PostList = ({type}) => {
         <div className="post-list-item" key={post[type === 'recommend' ? 'recommend_NO' : 'review_NO']}>
           <a href={`/${type === 'recommend' ? 'recommendPost' : 'reviewPost'}/${post[type === 'recommend' ? 'recommend_NO' : 'review_NO']}`} className="post-list-link">
             <div className="post-list-content">
+              <img src={post.fileUrl} alt={post[type === 'recommend' ? 'recommend_BOOKTITLE' : 'review_BOOKTITLE']} className="post-list-img" />         
               <div className="post-list-header">
-                <img src={post.fileUrl} alt={post[type === 'recommend' ? 'recommend_BOOKTITLE' : 'review_BOOKTITLE']} className="post-list-img" />
-                <div className="post-list-title">
-                  <div>{post[type === 'recommend' ? 'recommend_TITLE' : 'review_TITLE']}</div>
-                  <span className="post-list-time">작성일: {formatDate(post[type === 'recommend' ? 'recommend_TIME' : 'review_TIME'])}</span>
-                <span className="post-list-likes">좋아요 수: {post.likeCnt}</span>
-                <span className="post-list-comments">댓글 수: {post.cmtCnt}</span>
+                <div className="post-list-title"> {`[ ${post[type === 'recommend' ? 'recommend_TITLE' : 'review_TITLE']} ]`}</div>
+                <span className="post-list-time">작성일 : {formatDate(post[type === 'recommend' ? 'recommend_TIME' : 'review_TIME'])}</span>
+                <span className="post-list-postContent">내용 : {post[type === 'recommend' ? 'recommend_CONTENT' : 'review_CONTENT'].length > 20 
+                  ? `${post[type === 'recommend' ? 'recommend_CONTENT' : 'review_CONTENT'].substring(0, 20)}...` 
+                  : post[type === 'recommend' ? 'recommend_CONTENT' : 'review_CONTENT']}</span>
+                <div className="iconZone">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff9797" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                  </svg>
+                  <span className="post-list-likes">{post.likeCnt}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff9797" className="bi bi-chat-fill" viewBox="0 0 16 16">
+                    <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15"/>
+                  </svg>
+                  <span className="post-list-comments"><i className="fas fa-comment"></i> {post.cmtCnt}</span>
                 </div>
               </div>
-              {/* <div className="post-list-details">
-                <span className="post-list-time">작성일: {formatDate(post[type === 'recommend' ? 'recommend_TIME' : 'review_TIME'])}</span>
-                <span className="post-list-likes">좋아요 수: {post.likeCnt}</span>
-                <span className="post-list-comments">댓글 수: {post.cmtCnt}</span>
-              </div> */}
             </div>
           </a>
         </div>
       ))}
     </div>
   )
+  
 }
 export default PostList;
