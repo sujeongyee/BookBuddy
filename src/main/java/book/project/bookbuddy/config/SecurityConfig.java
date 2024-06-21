2600489bbc76216ee7ef6873960bfd98741d1c5e
ddu <sujeongyee0529@gmail.com>
2024-06-21 22:20:28 +0900

    delete 요청 오류 해결

--- src/main/java/book/project/bookbuddy/config/SecurityConfig.java
+++ src/main/java/book/project/bookbuddy/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package book.project.bookbuddy.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,13 +27,13 @@ public class SecurityConfig {
         return (web) -> web.ignoring().requestMatchers("/**"); // /book으로 들어오는 요청 허용
         //return (web) -> web.ignoring().requestMatchers("/book/**"); // /book으로 들어오는 요청 허용
     }
-
+    
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/book").allowedOrigins("http://localhost:3000").allowedMethods("*");
             }
         };
     }
--- src/main/java/book/project/bookbuddy/config/WebConfig.java
+++ src/main/java/book/project/bookbuddy/config/WebConfig.java
@@ -1,17 +1,44 @@
 package book.project.bookbuddy.config;
 
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 허용할 origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
-                .allowedHeaders("*") // 허용할 헤더
-                .allowCredentials(true); // 자격 증명 허용 여부
-    }
+
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //             .allowedOrigins("http://localhost:3000") 
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    //             .allowedHeaders("*") 
+    //             .allowCredentials(true); 
+    // }
+
+    // @Bean
+    // public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean(){
+    //     CorsConfiguration config = new CorsConfiguration();
+
+    //     config.setAllowCredentials(false);
+    //     config.addAllowedOrigin("*");
+    //     config.addAllowedMethod("*");
+    //     config.setMaxAge(6000L);
+
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();    
+    //     source.registerCorsConfiguration("/**", config);
+
+    //     FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));    
+    //     filterBean.setOrder(0);
+
+    //     return filterBean;
+    // }
+
+    
 }
