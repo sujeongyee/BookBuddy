6d7ee361daa9943f0ae5dfde0471c1b930737f17
ddu <sujeongyee0529@gmail.com>
2024-07-04 16:44:34 +0900

    비로그인 리뷰/추천 완성

--- src/main/java/book/project/bookbuddy/command/ReviewVO.java
+++ src/main/java/book/project/bookbuddy/command/ReviewVO.java
@@ -27,4 +27,9 @@ public class ReviewVO { // 리뷰게시글 vo
   private int review_RATING;
   private String book_ISBN;
   private String book_THUMBNAIL;
+  private Integer cmtCnt;
+  private Integer likeCnt;
+  private String fileUrl;
+  private String user_NICK;
+  private String profile_URL;
 }
